1cb384e1722541f6aa31f64f0c887d9147a6951e
Tarun kaushik <projects4tk@gmail.com>
2022-09-26 12:52:45 +0530

    Update rightViewOfBinaryTree.swift

--- Trees/rightViewOfBinaryTree.swift
+++ Trees/rightViewOfBinaryTree.swift
@@ -2,6 +2,37 @@
 // Input: root = [1,2,3,null,5,null,4]
 // Output: [1,3,4]
 
+
+/// simpler code
+
+class Solution {
+    func rightSideView(_ root: TreeNode?) -> [Int] {
+        var queue = [root]
+        var arr:[Int] = []
+        
+        while queue.count > 0 {
+            
+            if let rightMostNode = queue.last,let value = rightMostNode?.val {
+                arr.append(value)
+            }
+            
+            for node in queue {
+                if let left = node?.left {
+                    queue.append(left)
+                }
+                
+                if let right = node?.right {
+                    queue.append(right)
+                }
+                
+                queue.remove(at:0)
+            }
+        }
+        
+        return arr
+    }
+}
+
 class Solution {
     func rightSideView(_ root: TreeNode?) -> [Int] {
         var queue = [root]
